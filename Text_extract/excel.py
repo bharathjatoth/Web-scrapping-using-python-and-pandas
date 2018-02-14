8dd599f750738aeb9d28afe43df21a4472e0f7a9
Kittu <bharath72kumar@gmail.com>
2018-02-14 11:39:11 +0530

    Update excel.py

--- Text_extract/excel.py
+++ Text_extract/excel.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from pathlib import Path
 pd.set_option('display.max_colwidth', -1)
-pathlist = Path(r'C:\Users\jatoth.kumar\Desktop\Iape').glob('**/*.xlsx')
+pathlist = Path(pathlocation of list of files).glob('**/*.xlsx')
 for path in pathlist:
     x = str(path)
-    df = pd.read_excel(x,sheetname=None)
+    df = pd.read_excel(x,sheetname=None) #sheetname None So that I need to read all the workbooks in it
     print(path)
     print(df)
